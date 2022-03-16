f5993b2ec0864be1026de8111ad1146d7ae00940
Fajar Hikmal <fajarhikmal214@gmail.com>
2022-03-16 07:50:29 +0700

    Fix ESLint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,23 @@
 import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { google } from 'googleapis';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
-@Controller('v1/auth/users')
+@Controller('v1/auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  private oAuth2Client: any;
+
+  constructor(
+    private authService: AuthService,
+    private configService: ConfigService,
+  ) {
+    this.oAuth2Client = new google.auth.OAuth2(
+      this.configService.get('GOOGLE_CLIENT_ID'),
+      this.configService.get('GOOGLE_CLIENT_SECRET'),
+      this.configService.get('GOOGLE_CALLBACK_URL'),
+    );
+  }
 
   @Post('/sign-in')
   async signIn(
@@ -12,6 +25,17 @@ export class AuthController {
     @Res() response,
   ): Promise<any> {
     const data = await this.authService.signIn(authCredentialsDto);
+
+    return response.status(HttpStatus.OK).send({
+      statusCode: HttpStatus.OK,
+      message: 'User signed in successfully',
+      data,
+    });
+  }
+
+  @Post('/google/authenticate')
+  async authenticate(@Body() request, @Res() response): Promise<void> {
+    const data = await this.authService.authenticate(request);
 
     return response.status(HttpStatus.OK).send({
       statusCode: HttpStatus.OK,
