0a05c71211dd195ca88d30c15ce2b3bde2634f9d
Fajar Hikmal <fajarhikmal214@gmail.com>
2022-03-16 09:09:39 +0700

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
